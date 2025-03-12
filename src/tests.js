b462617b76fee204cf0da67c21d0138474b3fdbb
jack <jackfine01@gmail.com>
2025-03-12 12:07:30 -0400

    adding tests.js and tests.test.js files with empty test functions.

---
+++ src/tests.js
@@ -0,0 +1,24 @@
+function capitalize(){
+
+};
+module.exports = capitalize;
+
+function reverseString(){
+
+};
+module.exports = reverseString;
+
+function calculator(){
+
+};
+module.exports = calculator;
+
+function caesarCipher(){
+
+};
+module.exports = caesarCipher;
+
+function analyzeArray(){
+
+};
+module.exports = analyzeArray;
